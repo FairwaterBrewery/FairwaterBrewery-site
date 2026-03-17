939357265ec3d9f13de650dc9a9801fc2c362b8d
Bryan Robson <bryan@fairwater-brewery.co.uk>
2026-03-17 16:35:21 +0000

    Lowercase for the QR codes

--- _data/taplist.js
+++ _data/taplist.js
@@ -1,9 +1,9 @@
 export default {
-    wifiQr: "/img/qr/FairwaterBrewery WIFI QR.svg",
+    wifiQr: "/img/qr/fwbqr.svg",
 	wifiAlt: "QR code for connecting to the Fairwater Brewery wifi network",
-	taplistQr: "/img/qr/TaplistQR.png",
+	taplistQr: "/img/qr/taplistqr.png",
 	taplistAlt: "Direct link to this taplist page",
-	rabQr: "/img/qr/rabQR.png",
+	rabQr: "/img/qr/rabqr.png",
 	rabAlt: "Reading Amateur Brewers QR code - link to the RAB website",
     rabfestLogo: "/img/site/RABFest2026-1024x1024.jpeg",
     rabfestAlt: "Reading Amateur Brewers Festival 2026 logo - a stylised hop cone with the text RABFest 2026",
