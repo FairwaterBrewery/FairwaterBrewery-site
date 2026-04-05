c853afb345bec703035b696e6eef54f70c86d675
Bryan Robson <bryan@fairwater-brewery.co.uk>
2026-04-05 17:04:36 +0100

    Switch to Keezer

--- _data/keezer.js
+++ _data/keezer.js
@@ -1,26 +1,26 @@
 export default {
     tap1: {
-        name: "Dry ;-(",
-        colour: "",
-        style: "",
-        abv: "",
-        notes: "",
-        recipe: ""
+        name: "Ding-ish",
+        colour: "ebc-8",
+        style: "Munich Helles",
+        abv: "5.0%",
+        notes: "Based on Double-Barrelled's recipe for Ding in Andy Parker's The Modern Homebrewer. I didn't have any pilsener malt, so I used Simpson's Low Colour Maris Otter instead.",
+        recipe: "2026-02-04-ding-ish"
     },
     tap2: {
-        name: "Dry ;-(",
-        colour: "",
-        style: "",
-        abv: "",
-        notes: "",
+        name: "Oregonian",
+        colour: "ebc-8",
+        style: "West Coast IPA",
+        abv: "5.4%",
+        notes: "A variant on the Elusive Brewery classic pale ale!",
         recipe: ""
     },
     tap3: {
-        name: "Dry ;-(",
-        colour: "",
-        style: "",
-        abv: "",
-        notes: "",
+        name: "Vanilla & Run Porter",
+        colour: "ebc-60",
+        style: "Robust Porter",
+        abv: "6.0%",
+        notes: "Sweet vanilla and rum flavour complimented by subtle notes of winter berries and dried fruit.",
         recipe: ""
     },
     tap4: {
@@ -40,11 +40,11 @@ export default {
         recipe: ""
     },
     tap6: {
-        name: "Dry ;-(",
+        name: "Soda Water",
         colour: "",
         style: "",
-        abv: "",
-        notes: "",
+        abv: "0%",
+        notes: "Carbonated water...",
         recipe: ""
     }    
 }
--- _data/keggorator.js
+++ _data/keggorator.js
@@ -8,27 +8,27 @@ export default {
         recipe: ""
     },
     tap2: {
-        name: "Oregonian",
-        colour: "ebc-8",
-        style: "West Coast IPA",
-        abv: "5.4%",
-        notes: "A variant on the Elusive Brewery classic pale ale!",
+        name: "Dry ;-(",
+        colour: "",
+        style: "",
+        abv: "",
+        notes: "",
         recipe: ""
     },
     tap3: {
-        name: "Ding-ish",
-        colour: "ebc-8",
-        style: "Munich Helles",
-        abv: "5.0%",
-        notes: "Based on Double-Barrelled's recipe for Ding in Andy Parker's The Modern Homebrewer. I didn't have any pilsener malt, so I used Simpson's Low Colour Maris Otter instead.",
-        recipe: "2026-02-04-ding-ish"
+        name: "Dry ;-(",
+        colour: "",
+        style: "",
+        abv: "",
+        notes: "",
+        recipe: ""
     },
     tap4: {
-        name: "Vanilla & Run Porter",
-        colour: "ebc-60",
-        style: "Robust Porter",
-        abv: "6.0%",
-        notes: "Sweet vanilla and rum flavour complimented by subtle notes of winter berries and dried fruit.",
+        name: "Dry ;-(",
+        colour: "",
+        style: "",
+        abv: "",
+        notes: "",
         recipe: ""
     }
 }
